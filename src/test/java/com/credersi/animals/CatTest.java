40cba891b1dc4f9b40e6e21f65f197f6e8540a98
tommy.oladiti <tommy.oladiti@ROQ-322.roq.local>
2023-02-22 11:22:17 +0000

    Tests with private classes

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -12,4 +12,49 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	
+	@Test
+	public void runs() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		cat.feeds();
+		assertEquals(cat.isAlive(), true);
+		assertEquals(cat.howHungry(), 30);
+	}
+	
+	@Test
+	public void eating() {
+		Cat cat = new Cat();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+		assertEquals(cat.isAlive(), true);
+	}
+	
+	@Test
+	public void feedsrun() {
+		Cat cat = new Cat();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		cat.runs();
+		cat.runs();
+		cat.feeds();
+		cat.runs();
+		assertTrue(cat.isAlive());
+		assertEquals(cat.howHungry(), 70);
+	}
+	
+	@Test
+	public void feeds() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 18; i++) {
+			cat.runs();
+		}
+		assertEquals(cat.isAlive(), false);
+	}
 }
